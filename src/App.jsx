3482f42b62fcc82c4af9588e63146471fbc26636
Cinnamon <213962814+irunonironic@users.noreply.github.com>
2025-07-17 22:13:48 +0530

    add info section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Header from './Components/HeaderAnnounce'
 import Header2 from './Components/Header2/HeadMain'
 import HeadMain from './Components/Header2/HeadMain'
 import Body from './Components/Body'
+import Info from './Components/Info'
 const App = () => {
   return (
     <div >
       <Header />
       <HeadMain />
     <Body />
-      
+      <Info />
     </div>
   )
 }
---
+++ src/Components/Info.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='text-[3rem] font-[520] font-[Gotham] text-[#12703c] leading-[2] pt-4 pl-28 tracking-wider '>
+      Multi-award winning
+    </div>
+  )
+}
+
+export default Info
